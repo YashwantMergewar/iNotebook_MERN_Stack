ac5a022680345835e2fa2eca00388dd3e729097e
yashwant mergewar <yashwantmergewar@gmail.com>
2025-03-10 20:46:10 +0530

    validation are use here

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017"
+const mongoURI = "mongodb://localhost:27017/inotebookDB"
 
 async function connectToMongo(){
     try {
@@ -7,7 +7,7 @@ async function connectToMongo(){
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB Successfully');
     }catch (error) {
         console.error('MongoDB connection error:', error);
         process.exit(1); // Exit process on failure
